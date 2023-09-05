a6fcfd31716957ef7cc4d21a730fce1e633ade22
Rick <101254647+nauish@users.noreply.github.com>
2023-09-05 16:48:02 +0800

    Import functions written in another project

---
+++ main.js
@@ -0,0 +1,37 @@
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const sum = function (array) {
+  return array.reduce((total, current) => {
+    return total + current;
+  }, 0);
+};
+
+const multiply = function (array) {
+  return array.reduce((total, current) => {
+    return total * current;
+  }, 0);
+};
+
+const power = function (base, power) {
+  return base ** power;
+};
+
+const factorial = function (int) {
+  if (int === 0 || int === 1) return 1;
+  else return int * factorial(int - 1);
+};
+
+const factorialLoop = function (int) {
+  let answer = 1;
+  if (int === 0 || int === 1) return 1;
+  for (let i = int; i > 0; i--) {
+    answer *= i;
+  }
+  return answer;
+};
