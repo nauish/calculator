7cc8055071f8a0e3625a2021c43d38847a6f0e51
Rick <101254647+nauish@users.noreply.github.com>
2023-09-06 20:36:00 +0800

    Add a number to the list to avoid weird JS quirks

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ class Calculator {
     this.currentOperant = this.currentOperant.toString() + number.toString();
     if (
       this.currentOperant[0] == "0" &&
-      this.currentOperant[1] in [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      this.currentOperant[1] in [1, 2, 3, 4, 5, 6, 7, 8, 9, 9]
     ) {
       this.currentOperant = this.currentOperant.slice(1);
     }
